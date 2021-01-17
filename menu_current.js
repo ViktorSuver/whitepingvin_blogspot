da8f11cefc66819b8903420703da7978a5dacb3f
suvernet <74303408+suvernet@users.noreply.github.com>
2021-01-17 20:27:05 +0300

    Update menu_current.js

--- menu_current.js
+++ menu_current.js
@@ -1,4 +1,4 @@
-		(function () {
+		$(function () {
         var a = document.querySelectorAll('.menu li a');
           for (var i=a.length; i--;) {
          if (a[i].href === window.location.pathname || a[i].href === window.location.href) a[i].className += ' active';
